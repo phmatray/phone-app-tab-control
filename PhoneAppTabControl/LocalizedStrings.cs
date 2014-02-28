2c3b48a058f9dec48c693c80d244ea2f3f10c9f4
Philippe Matray <phmatray@outlook.com>
2014-03-01 00:42:00 +0100

    Publication de la solution

---
+++ PhoneAppTabControl/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneAppTabControl.Resources;
+
+namespace PhoneAppTabControl
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ PhoneAppTabControl/PivotPage1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PhoneAppTabControl
+{
+    public partial class PivotPage1 : PhoneApplicationPage
+    {
+        public PivotPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            pivot.SelectedIndex = 0;
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            pivot.SelectedIndex = 0;
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            pivot.SelectedIndex = 1;
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+            pivot.SelectedIndex = 2;
+        }
+
+        private void bt4_Click(object sender, RoutedEventArgs e)
+        {
+            pivot.SelectedIndex = 3;
+        }
+
+        private void bt5_Click(object sender, RoutedEventArgs e)
+        {
+            pivot.SelectedIndex = 4;
+        }
+
+        private void pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string stateName = String.Format("VisualState{0}", pivot.SelectedIndex+1);
+            VisualStateManager.GoToState(this, stateName, true);
+        }
+    }
+}
